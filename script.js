fe7a82ce05670b74885f4afb0b3fd98d5597c1ff
pavanlupane <pavanlupane@gmail.com>
2015-12-31 16:19:15 -0800

    version 2.4 skills meter fix with travel div title

--- script.js
+++ script.js
@@ -16,14 +16,21 @@ $(function() {
     }); 
     
     var myRepeatFunction = function (){
-                if($("#skillsDivContainer").offset().top < $(window).scrollTop() + $(window).outerHeight()) {
-                    //console.log('True');
+               if(($("#skillsDivContainer").offset().top < $(window).scrollTop() + $(window).outerHeight() - 200) &&
+                      ($("#travelDiv").offset().top > $(window).scrollTop() + $(window).outerHeight()-400)) {
+                    console.log('True');
+                    console.log('Div top value::' + $("#skillsDivContainer").offset().top );
+                    console.log('travelDiv top value::' + $("#travelDiv").offset().top );
+                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight()- 200));
                     $('#skillsMeter').show();
                     
-                } else {
-                   // console.log('False');
+                }  else {
+                   console.log('False');
+                    console.log('Div top value::' + $("#skillsDivContainer").offset().top );
+                    console.log('travelDiv top value::' + $("#travelDiv").offset().top );
+                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight() - 200));
                     $('#skillsMeter').hide();
                 }
             };
-            setInterval(myRepeatFunction, 300);
+            setInterval(myRepeatFunction, 200);
 });

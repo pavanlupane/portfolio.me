8603d0bdf41e7045d8656693b9993d45c06700b4
pavanlupane <pavanlupane@gmail.com>
2016-01-01 23:28:50 -0800

    version 2.4.1 contact tab till form design

--- script.js
+++ script.js
@@ -18,19 +18,43 @@ $(function() {
     var myRepeatFunction = function (){
                if(($("#skillsDivContainer").offset().top < $(window).scrollTop() + $(window).outerHeight() - 200) &&
                       ($("#travelDiv").offset().top > $(window).scrollTop() + $(window).outerHeight()-400)) {
-                    console.log('True');
+                    /*console.log('True');
                     console.log('Div top value::' + $("#skillsDivContainer").offset().top );
                     console.log('travelDiv top value::' + $("#travelDiv").offset().top );
-                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight()- 200));
+                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight()- 200));*/
                     $('#skillsMeter').show();
                     
                 }  else {
-                   console.log('False');
+                   /*console.log('False');
                     console.log('Div top value::' + $("#skillsDivContainer").offset().top );
                     console.log('travelDiv top value::' + $("#travelDiv").offset().top );
-                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight() - 200));
+                    console.log('The other value::'+ ($(window).scrollTop() + $(window).outerHeight() - 200));*/
                     $('#skillsMeter').hide();
                 }
             };
             setInterval(myRepeatFunction, 200);
+    
+function mapInitialize() {
+
+    var mapCanvas = document.getElementById('googleMap');
+    var myLatLng = {lat: 34.032726, lng: -118.282182};
+    var mapOptions = {
+      center: new google.maps.LatLng(34.032726, -118.282182),
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: mapCanvas,
+    animation: google.maps.Animation.DROP,
+    title: 'Pavan Lupane'
+  });
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    marker.setMap(map);
+
+}
+
+//google.maps.event.addDomListener(window, 'load', mapInitialize);
+    mapInitialize();
 });
+
